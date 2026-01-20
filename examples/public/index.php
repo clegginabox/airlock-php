a5cf912f3edad6050f35130ef1649da088539316
Paul Clegg <pclegg@gmail.com>
2026-01-20 23:00:28 +0000

    adding codacy ignore

--- examples/public/index.php
+++ examples/public/index.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $kernel = new Kernel('dev', true);
 $request = Request::createFromGlobals();
