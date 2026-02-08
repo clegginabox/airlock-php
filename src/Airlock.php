67ce87396f413ed8eb2ce3a247f3239a4164577b
Paul Clegg <hello@clegginabox.co.uk>
2026-02-08 13:14:31 +0000

    updating phpunit

--- src/Airlock.php
+++ src/Airlock.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Clegginabox\Airlock;
 
-use Clegginabox\Airlock\Seal\SealToken;
-
 /**
  * Main entry point for managing access to a capacity-limited resource.
  *
